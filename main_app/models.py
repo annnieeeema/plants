901dc339bd250d2d82bef8463e4e241adbce12b4
Annie Ma <anniema@Annies-MacBook-Pro.local>
2022-07-13 16:52:09 -0400

    addition of plant index and cards

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    type = models.CharField(max_length=50)
+    difficultyLevel = models.IntegerField()
+    waterFrequency = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Plant
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def plants_index(request):
+    plants = Plant.objects.all()
+    return render(request, 'plants/index.html', {'plants':plants})
